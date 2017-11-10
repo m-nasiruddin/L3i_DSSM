7ab5191a0f04fe7fe342adfc5c75c1324021d148
Mohammad Nasiruddin <mohammadnasiruddin@users.noreply.github.com>
2017-11-10 17:24:23 +0100

    Update LTG.py

--- LTG.py
+++ LTG.py
@@ -2,21 +2,24 @@
 """
 Created on Mon Nov  6 11:16:35 2017
 
+This Python script converts a corpus into letter-tri-gram (LGT) format.
+ 
 @author: mohammad
 """
 
 #!/usr/bin/python
 
+import codecs
 import re
 
-str = "Tom Cruise's movies."
-
-reg = re.sub(r'[^A-Za-z0-9]', '', str)
-
-reg_lwr = reg.lower()
-
-new_str = '#' + reg_lwr + '#'
-
-tri_gram = [new_str[i:i+3] for i in range(len(new_str)-3+1)]
-
-print (tri_gram)
+source = codecs.open('data/input/corpus.txt', 'r', 'utf-8')  # open the input
+                                                    # corpus from the  path
+corpus = source.readlines()  # read the entire corpus at once
+for line in corpus:  # loop the corpus line by line
+    reg = re.sub(r'[^A-Za-z0-9]', '', line)  # remove all the symbols other
+                        # than the 26 English letters a-Z and the 10 digits 0-9
+    reg_lwr = reg.lower()  # change all the letters to lower case
+    new_str = '#' + reg_lwr + '#'  # add '#' at the head and the tail of the
+                                                                # text stream
+    tri_gram = [new_str[i:i+3] for i in range(len(new_str)-3+1)] # make the LGT
+    print(tri_gram)
